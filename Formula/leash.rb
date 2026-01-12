b714f0f0b3ef4c614be035a7bd7e778c21019032
Navan Chauhan <navan.chauhan@strongdm.com>
2026-01-12 09:52:41 -0800

    chore(brew): bump leash formula to version 1.1.5

--- Formula/leash.rb
+++ Formula/leash.rb
@@ -1,26 +1,26 @@
 class Leash < Formula
   desc "Security, visibility, and authorization for AI agents"
   homepage "https://github.com/strongdm/leash"
-  version "1.1.3"
+  version "1.1.5"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/strongdm/leash/releases/download/v#{version}/leash_#{version}_darwin_arm64.tar.gz"
-      sha256 "9c37785c70104a86f714abd9f179e97246126fe79ed3dea9ceea2a0d5ac6e0d3"
+      sha256 "30854babe64de74a7c0c3fbfa3abb224b11bcd1edd81c45787c8ef9d022401ef"
     else
       url "https://github.com/strongdm/leash/releases/download/v#{version}/leash_#{version}_darwin_amd64.tar.gz"
-      sha256 "a7c51454c3982169ff963b86c63308dfc533113a1c7eaa455efd63464073608c"
+      sha256 "a492d11bb1e3cfbc3cf86d7f40ece72d30e279bf0fbf89087faba8473b57d6a7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/strongdm/leash/releases/download/v#{version}/leash_#{version}_linux_arm64.tar.gz"
-      sha256 "537b0affd8169b9b498f22722fca44dea1a3b0e4e1745e38516d5e2bdeb74a83"
+      sha256 "b5c790ff25e6540f2abec72cca7e53da3b119362e7ad3a874f55183b63d30640"
     else
       url "https://github.com/strongdm/leash/releases/download/v#{version}/leash_#{version}_linux_amd64.tar.gz"
-      sha256 "e17492ee92998dfa4b28c06054fa37813f4ae7d7d9bc18a208d3b47792f0e960"
+      sha256 "62e644553133c34d79f0fe6d0bd3ac685ae7f1210f0c1bea8a9c0f60ccd170da"
     end
   end
 
